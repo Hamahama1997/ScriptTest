b333f23348cdeb739e8e5b3a3c0ec83cfd0e9e7d
Hamokuko1409 <hamahama1997@gmail.com>
2019-01-29 02:46:51 +0900

    Lesson4の課題
    ・要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化
    ・for文を使い、配列の各要素の値を順番に表示
    ・for文を使い、配列の各要素の値を逆順に表示

---
+++ Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        for(int a = 0; a < array.Length; a++)
+        {
+            Debug.Log(array[a]);
+        }
+
+        for (int b = array.Length - 1; 0 <= b; b--)
+        {
+            Debug.Log(array[b]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
